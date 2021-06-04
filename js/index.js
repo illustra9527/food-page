248b7136c37433c36d5ae2eaab0424e6b66e3a88
illustra9527 <yabesoube@gmail.com>
2021-06-05 01:32:09 +0800

    [修改] 新增動畫及功能
    新增 AOS 動畫，理念與產品標題，to top 按鈕

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Swiper init */
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -10,4 +11,21 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+/* AOS init */
+AOS.init();
+
+/* go to top */
+function goTop() {
+    let scrollH = document.documentElement.scrollTop;
+    if (scrollH >= 200) {
+        $('.go-top').addClass('visible');
+    } else {
+        $('.go-top').removeClass('visible');
+    }
+}
+
+window.addEventListener('scroll', function () {
+    goTop();
 });
